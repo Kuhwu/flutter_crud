4284398179695fd12e1f8a5958093afae6cc022b
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-08-24 19:40:37 +0800

    NEW UPDATE
    Create Done For Android
        - UI
        - API

--- lib/createpage.dart
+++ lib/createpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/services/api.dart';
 
 class CreatePage extends StatefulWidget {
   const CreatePage({super.key});
@@ -12,9 +13,9 @@ class _CreatePageState extends State<CreatePage> {
 
   var firstNameController = TextEditingController();
   var lastnameController = TextEditingController();
-  var courseController = TextEditingController();
   var yearController = TextEditingController();
-  var enrollerController = TextEditingController();
+  String? selectedCourse;
+  //var enrolledController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +56,8 @@ class _CreatePageState extends State<CreatePage> {
                   ),
                   Container(
                     margin: const EdgeInsets.only(left: 70, right: 70),
-                    child: const TextField(
+                    child: TextField(
+                      controller: lastnameController,
                       style: TextStyle(color: Colors.black),
                       decoration: InputDecoration(
                         hintText: 'Last Name',
@@ -71,8 +73,24 @@ class _CreatePageState extends State<CreatePage> {
                   ),
                   Container(
                     margin: const EdgeInsets.only(left: 70, right: 70),
-                    child: const TextField(
-                      style: TextStyle(color: Colors.black),
+                    child: DropdownButtonFormField<String>(
+                      value: selectedCourse,
+                      items: <String>[
+                        'BSCE',
+                        'BSIT',
+                        'BSBA',
+                        'BSED',
+                        'BSARCH',
+                        'BSCRIM',
+                      ].map((String year) {
+                        return DropdownMenuItem<String>(
+                          value: year,
+                          child: Text(
+                            year,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        );
+                      }).toList(),
                       decoration: InputDecoration(
                         hintText: 'Course',
                         hintStyle: TextStyle(color: Colors.black),
@@ -83,11 +101,18 @@ class _CreatePageState extends State<CreatePage> {
                           borderSide: BorderSide(color: Colors.black),
                         ),
                       ),
+                      dropdownColor: Colors.white,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedCourse = newValue;
+                        });
+                      },
                     ),
                   ),
                   Container(
                     margin: const EdgeInsets.only(left: 70, right: 70),
-                    child: const TextField(
+                    child: TextField(
+                      controller: yearController,
                       style: TextStyle(color: Colors.black),
                       decoration: InputDecoration(
                         hintText: 'Year',
@@ -102,28 +127,43 @@ class _CreatePageState extends State<CreatePage> {
                     ),
                   ),
                   Container(
-                      margin: const EdgeInsets.only(left: 70, right: 70),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Enrolled',
-                            style: TextStyle(color: Colors.black, fontSize: 16),
-                          ),
-                          Switch(
-                            value: isEnrolled,
-                            onChanged: (bool value) {
-                              setState(() {
-                                isEnrolled = value;
-                              });
-                            },
-                            activeColor: Colors.black,
-                            activeTrackColor: Colors.green.shade400,
-                            inactiveThumbColor: Colors.black,
-                            inactiveTrackColor: Colors.red.shade400,
-                          ),
-                        ],
-                      ))
+                    margin: const EdgeInsets.only(left: 70, right: 70),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Enrolled',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                        Switch(
+                          value: isEnrolled,
+                          onChanged: (bool value) {
+                            setState(() {
+                              isEnrolled = value;
+                            });
+                          },
+                          activeColor: Colors.black,
+                          activeTrackColor: Colors.green.shade400,
+                          inactiveThumbColor: Colors.black,
+                          inactiveTrackColor: Colors.red.shade400,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 20)),
+                  ElevatedButton(
+                      onPressed: () {
+                        var data = {
+                          "firstName": firstNameController.text,
+                          "lastName": lastnameController.text,
+                          "year": yearController.text,
+                          "course": selectedCourse,
+                          "enrolled": isEnrolled,
+                        };
+
+                        Api.addStudent(data);
+                      },
+                      child: Text("Submit")),
                 ],
               ),
             ),
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -4,16 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  static const baseUrl = "http://192.168.0.8/api/";
+  static const baseUrl = "http://192.168.0.9:3000/api/";
 
   static addStudent(Map sdata) async {
+    sdata['enrolled'] = sdata['enrolled'].toString();
+
+    print(sdata);
     var url = Uri.parse(baseUrl + "add_student");
 
     try {
       final res = await http.post(url, body: sdata);
       if (res.statusCode == 200) {
         var data = jsonDecode(res.body.toString());
-        print(data);
+        print(sdata);
       } else {
         print("Upload Failed");
       }
